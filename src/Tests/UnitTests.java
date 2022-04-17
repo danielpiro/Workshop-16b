589daca184fb0e69d182650bd8441b32698466f1
amitpeled92 <62392209+amitpeled92@users.noreply.github.com>
2022-04-17 13:12:06 +0300

    building tests env

---
+++ src/Tests/UnitTests.java
@@ -0,0 +1,25 @@
+package Tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnitTests {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void first_test() {
+        assertTrue(1 == 1);
+    }
+
+
+}
