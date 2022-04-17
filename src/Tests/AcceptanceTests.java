4e5447e687973d12266b8d8a8ace5659c24fde6f
amitpeled92 <62392209+amitpeled92@users.noreply.github.com>
2022-04-17 14:59:19 +0300

    Start buiding tests

---
+++ src/Tests/AcceptanceTests.java
@@ -0,0 +1,24 @@
+package Tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AcceptanceTests {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void first_test() {
+        assertTrue(1 == 1);
+    }
+
+}
---
+++ src/Tests/IntegrationTests.java
@@ -0,0 +1,24 @@
+package Tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IntegrationTests {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void first_test() {
+        assertTrue(1 == 1);
+    }
+
+}
