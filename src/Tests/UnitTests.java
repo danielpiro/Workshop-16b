d615aeb1ab332b63ecdf3f9921f074d0071f9c36
amitpeled92 <62392209+amitpeled92@users.noreply.github.com>
2022-04-19 13:21:40 +0300

    Update UnitTests.java

--- src/Tests/UnitTests.java
+++ src/Tests/UnitTests.java
@@ -1015,6 +1015,149 @@ class UnitTests {
                 """);
         fail();
     }
+    @Test
+    void adding_store_owner_fail_case_test1() {
+        System.out.println("""
+                - Check that the user (adding the new store owner) is logged in as store owner.
+                    -> the user isn't logged in as a store owner.
+                - Show fail message...
+                """);
+        fail();
+    }
+    @Test
+    void adding_store_owner_fail_case_test2() {
+        System.out.println("""
+                - Check that the user (adding the new store owner) is logged in as store owner.
+                - User inserting the new store owner details.
+                - Check that the new owner is registered to the system and not already a store owner
+                    -> the new owner inserted isn't a user in the system
+                - Show fail message...
+                """);
+        fail();
+    }
+    @Test
+    void adding_store_owner_fail_case_test3() {
+        System.out.println("""
+                - Check that the user (adding the new store owner) is logged in as store owner.
+                - User inserting the new store owner details.
+                - Check that the new owner is registered to the system and not already a store owner
+                    -> the new owner inserted is already a store owner
+                - Show fail message...
+                """);
+        fail();
+    }
+    @Test
+    void adding_store_owner_success_case_test4() {
+        System.out.println("""
+                - Check that the user (adding the new store owner) is logged in as store owner.
+                - User inserting the new store owner details.
+                - Check that the new owner is registered to the system and not already a store owner.
+                - Adding store owner to the store is activated.
+                - Check that the new owner has been added (will be FALSE).
+                """);
+        fail();
+    }
+
+    /**
+     *  User requirement - II.4.6
+     **/
+    @Test
+    void adding_store_manager_success_case_test() {
+        System.out.println("""
+                - Check that the user (adding the new store manager) is logged in as store owner.
+                - User inserting the new store manager details.
+                - Check that the new owner is registered to the system and not already a store owner or store manager.
+                - Adding store manager to the store is activated.
+                - Check that the new manager has been added (will be true).
+                """);
+        fail();
+    }
+    @Test
+    void adding_store_manager_fail_case_test1() {
+        System.out.println("""
+                - Check that the user (adding the new store owner) is logged in as store owner.
+                    -> the user isn't logged in as a store owner.
+                - Show fail message...
+                """);
+        fail();
+    }
+    @Test
+    void adding_store_manager_fail_case_test2() {
+        System.out.println("""
+                - Check that the user (adding the new store manager) is logged in as store owner.
+                - User inserting the new store manager details.
+                - Check that the new owner is registered to the system and not already a store owner or store manager
+                    -> the new manager inserted isn't a user in the system
+                - Show fail message...
+                """);
+        fail();
+    }
+    @Test
+    void adding_store_manager_fail_case_test3() {
+        System.out.println("""
+                - Check that the user (adding the new store manager) is logged in as store owner.
+                - User inserting the new store manager details.
+                - Check that the new manager is registered to the system and not already a store owner or store manager
+                    -> the new owner inserted is already a store owner or store manager
+                - Show fail message...
+                """);
+        fail();
+    }
+    @Test
+    void adding_store_manager_success_case_test4() {
+        System.out.println("""
+                - Check that the user (adding the new store manager) is logged in as store owner.
+                - User inserting the new store manager details.
+                - Check that the new manager is registered to the system and not already a store owner or store manager.
+                - Adding store manager to the store is activated.
+                - Check that the new manager has been added (will be FALSE).
+                """);
+        fail();
+    }
+
+    /**
+     *  User requirement - II.4.7
+     **/
+    @Test
+    void change_store_manager_permissions_success_case_test() {
+        System.out.println("""
+                - Check that the user (changing the store manager permissions) is logged in as store owner.
+                - User changing the store manager permissions.
+                - Check that the manager's permissions has been updated (will be true).
+                """);
+        fail();
+    }
+    @Test
+    void change_store_manager_permissions_fail_case_test1() {
+        System.out.println("""
+                - Check that the user (changing the store manager permissions) is logged in as store owner.
+                    -> the user isn't logged in as a store owner.
+                - Show fail message...
+                """);
+        fail();
+    }
+    @Test
+    void change_store_manager_permissions_fail_case_test2() {
+        System.out.println("""
+                - Check that the user (changing the store manager permissions) is logged in as store owner.
+                - User changing the store manager permissions.
+                - Check that the manager's permissions has been updated (will be FALSE).
+                """);
+        fail();
+    }
+    /**
+     *  User requirement - II.4.8
+     **/
+    @Test
+    void close_store_success_case_test() {
+        System.out.println("""
+                - Check that the user (changing the store manager permissions) is logged in as store owner.
+                - User changing the store manager permissions.
+                - Check that the manager's permissions has been updated (will be true).
+                """);
+        fail();
+    }
+
 
 //endregion User requirements
 
