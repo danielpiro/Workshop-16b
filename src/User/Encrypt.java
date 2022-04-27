fddea14b21b793fb6047243b90cef97f62550269
taweela <taweela@post.bgu.ac.il>
2022-04-27 11:36:22 +0300

    added string builder

--- src/User/Encrypt.java
+++ src/User/Encrypt.java
@@ -3,23 +3,22 @@ package User;
 public class Encrypt {
 
     public String encrypt(String to_encrypt) {
-        //todo StringBuilder
 
         char[] chars = to_encrypt.toCharArray();
-        String str ="";
+        StringBuilder st = new StringBuilder();
         for(char c: chars){
-            str += (char)(c+5);
+            st.append((char) (c + 5));
         }
-        return str;
+        return st.toString();
     }
 
     public String decrypt(String to_decrypt) {
         char[] chars1 = to_decrypt.toCharArray();
-        String str = "";
+        StringBuilder st = new StringBuilder();
         for (char c : chars1) {
-            str += (char)(c - 5);
+            st.append((char) (c + 5));
         }
-        return str;
+        return st.toString();
     }
 
     }
