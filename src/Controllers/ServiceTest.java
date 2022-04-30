0ceb347b59862977d8dc17d5021beedbb18bfa45
guy <guypora@post.bgu.ac.il>
2022-04-30 22:38:25 +0300

    added test

--- src/Controllers/ServiceTest.java
+++ src/Controllers/ServiceTest.java
@@ -56,7 +56,10 @@ class ServiceTest {
             Future per1 = service.purchaseCart(userId1, PaymentNames.Visa, DeliveryNames.FedEx);
             Future per2 = service.purchaseCart(userId2, PaymentNames.Visa, DeliveryNames.FedEx);
 
-            assertTrue((float)per1.get()== -1 ||  (float)per2.get()== -1 );
+
+            float x= (float)per1.get();
+            float x2= (float)per2.get();
+            assertTrue( (float)per1.get()== -1 ||  (float)per2.get()== -1 );
 
 
 
@@ -87,6 +90,42 @@ class ServiceTest {
             boolean b1  = (boolean)service.createOwner(storeId,userId1,userId3,per).get();
             boolean b2  =  (boolean)service.createOwner(storeId,userId1,userId3,per).get();
             assertTrue(b1^b2);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+
+    }
+
+    @Test
+    void OwnerDeleteProductWhileCustomerBuyIt() {
+        try {
+            String userId1 = "user1";
+            String userId2 = "user2";
+
+
+            service.sign_up(userId1, "123").get();
+            service.sign_up(userId2, "123").get();
+
+
+            service.login(userId1, "123").get();
+            service.login(userId2, "123").get();
+
+            String storeId = (String) service.openNewStore(userId1, "store1").get();
+            service.addNewProductToStore(storeId, userId1, "fuck", 1F, 1, "Other").get();
+            Future<HashMap<String, List<Product>>> storesAndProducts = service.getAllProductsAndStores(userId1);
+            String pID1 = storesAndProducts.get().get(storeId).get(0).getId();
+            System.out.println(pID1);
+            service.addProductFromCart(userId2,pID1,storeId,1,false).get();
+
+
+            Future p1= service.purchaseCart(userId2, PaymentNames.Visa,DeliveryNames.FedEx);
+            Future success = service.deleteProduct(storeId,userId1, pID1);
+
+            assertTrue(((float)p1.get()==-1 && (boolean)success.get()) || ((float)p1.get()!=-1 && !(boolean)success.get())  );
+
+
 
         } catch (Exception e) {
             e.printStackTrace();
