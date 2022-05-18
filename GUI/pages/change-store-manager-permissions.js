1b43373e62634bd3ba5da43c5995eb31ff96c5ed
amitpeled92 <62392209+amitpeled92@users.noreply.github.com>
2022-05-18 23:23:52 +0300

    Update change-store-manager-permissions.js

--- GUI/pages/change-store-manager-permissions.js
+++ GUI/pages/change-store-manager-permissions.js
@@ -9,6 +9,11 @@ const ChangeStoreManagerPermissions = () => {
     // const [isLoading, setIsLoading] = useState(true);
     // const [searchValue, setSearchValue] = useState("");
     const [userPermission, setUserPermission] = useState("Admin"); //TODO: Need to change to Guest when logic is ready!
+    const [permission, setPermissions] = useState({
+        p1: true, p2: true, p3: true, p4: true,
+        p5: true, p6: true, p7: true, p8: true,
+        p9: true, p10: true, p11: true, p12: true,
+    })
     // useEffect(() => {
     //   const fetchApi = async () => {
     //     const response = await axios.get("https://fakestoreapi.com/products");
@@ -19,6 +24,22 @@ const ChangeStoreManagerPermissions = () => {
     //   };
     //   fetchApi();
     // }, []);
+
+    useEffect(() => {
+        const fetchPermission = async () => {
+          const response = await axios.get("users/getUserPermission");
+          setUserPermission(response.data);
+        };
+        fetchPermission();
+    }, []);
+
+    const onUpdatePermissions = (e) => {
+        e.preventDefault();
+        document.getElementById("Permission1").checked= true;
+        document.getElementById("Permission1").disabled= true;
+        document.getElementById("Permission2").checked= true;
+        document.getElementById("Permission3").checked= true;
+    }
   
     var menu;
     if (userPermission == "Admin"){
@@ -73,47 +94,55 @@ const ChangeStoreManagerPermissions = () => {
                 <label className="form-check-label" for="flexSwitchCheckCheckedDisabled">Disabled checked switch checkbox input</label>
                 </div> */}
                 <div className="form-check form-switch">
-                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked/>
-                    <label className="form-check-label" for="flexSwitchCheckDefault">Permission #1</label>
+                    <input className="form-check-input" type="checkbox" id="Permission1"/>
+                    <label className="form-check-label" for="flexSwitchCheckDefault">View store history</label>
                 </div>
                 <div className="form-check form-switch">
-                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked" />
-                    <label className="form-check-label" for="flexSwitchCheckChecked">Permission #2</label>
+                    <input className="form-check-input" type="checkbox" id="Permission2"/>
+                    <label className="form-check-label" for="flexSwitchCheckChecked">Edit existing product</label>
                 </div>
                 <div className="form-check form-switch">
-                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked/>
-                    <label className="form-check-label" for="flexSwitchCheckDefault">Permission #3</label>
+                    <input className="form-check-input" type="checkbox" id="Permission3"/>
+                    <label className="form-check-label" for="flexSwitchCheckDefault">Add new product</label>
                 </div>
                 <div className="form-check form-switch">
-                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked" />
-                    <label className="form-check-label" for="flexSwitchCheckChecked">Permission #4</label>
+                    <input className="form-check-input" type="checkbox" id="Permission4"/>
+                    <label className="form-check-label" for="flexSwitchCheckChecked">Edit product</label>
                 </div>
                 <div className="form-check form-switch">
-                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked/>
-                    <label className="form-check-label" for="flexSwitchCheckDefault">Permission #5</label>
+                    <input className="form-check-input" type="checkbox" id="Permission5"/>
+                    <label className="form-check-label" for="flexSwitchCheckDefault">Remove product</label>
                 </div>
                 <div className="form-check form-switch">
-                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked" />
-                    <label className="form-check-label" for="flexSwitchCheckChecked">Permission #6</label>
+                    <input className="form-check-input" type="checkbox" id="Permission6"/>
+                    <label className="form-check-label" for="flexSwitchCheckChecked">Edit store discount</label>
                 </div>
                 <div className="form-check form-switch">
-                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked/>
-                    <label className="form-check-label" for="flexSwitchCheckDefault">Permission #7</label>
+                    <input className="form-check-input" type="checkbox" id="Permission7"/>
+                    <label className="form-check-label" for="flexSwitchCheckDefault">Add review to product</label>
                 </div>
                 <div className="form-check form-switch">
-                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked" />
-                    <label className="form-check-label" for="flexSwitchCheckChecked">Permission #8</label>
+                    <input className="form-check-input" type="checkbox" id="Permission8"/>
+                    <label className="form-check-label" for="flexSwitchCheckChecked">View forum</label>
                 </div>
                 <div className="form-check form-switch">
-                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked/>
-                    <label className="form-check-label" for="flexSwitchCheckDefault">Permission #9</label>
+                    <input className="form-check-input" type="checkbox" id="Permission9"/>
+                    <label className="form-check-label" for="flexSwitchCheckDefault">Reply to forum</label>
                 </div>
                 <div className="form-check form-switch">
-                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked" />
-                    <label className="form-check-label" for="flexSwitchCheckChecked">Permission #10</label>
+                    <input className="form-check-input" type="checkbox" id="Permission10"/>
+                    <label className="form-check-label" for="flexSwitchCheckChecked">Close store</label>
+                </div>
+                <div className="form-check form-switch">
+                    <input className="form-check-input" type="checkbox" id="Permission11"/>
+                    <label className="form-check-label" for="flexSwitchCheckChecked">Open store</label>
+                </div>
+                <div className="form-check form-switch">
+                    <input className="form-check-input" type="checkbox" id="Permission12"/>
+                    <label className="form-check-label" for="flexSwitchCheckChecked">Info of managers</label>
                 </div>
 
-                <button className="btn btn-primary mr-lg-3" style={{width: "20%"}}>
+                <button className="btn btn-primary mr-lg-3" style={{width: "20%"}} onClick={onUpdatePermissions}>
                     Update Permissions
                 </button>
             </div>
