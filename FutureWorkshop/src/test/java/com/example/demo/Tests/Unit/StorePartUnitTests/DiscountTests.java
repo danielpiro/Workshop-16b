d4a6baf181166a4484f0eb17b93b59dbdbf90c96
guy <guypora@post.bgu.ac.il>
2022-05-22 09:23:01 +0300

    a

--- FutureWorkshop/src/test/java/com/example/demo/Tests/Unit/StorePartUnitTests/DiscountTests.java
+++ FutureWorkshop/src/test/java/com/example/demo/Tests/Unit/StorePartUnitTests/DiscountTests.java
@@ -1,4 +1,26 @@
 package com.example.demo.Tests.Unit.StorePartUnitTests;
 
+import com.example.demo.CustomExceptions.Exception.SupplyManagementException;
+import com.example.demo.Store.InventoryManager;
+import com.example.demo.Store.ProductsCategories;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DiscountTests {
+    InventoryManager invMan =  new InventoryManager();
+    String Product1Id;
+    String Product2Id;
+    String Product3Id;
+    String Product4Id;
+    @BeforeEach
+    void setUp() throws SupplyManagementException {
+        invMan = new InventoryManager();
+        Product1Id = invMan.addNewProduct("t1", 5.5F, 4, ProductsCategories.Apps$Games.toString());
+        Product2Id = invMan.addNewProduct("t2", 1F, 1, ProductsCategories.Apps$Games.toString());
+        Product3Id = invMan.addNewProduct("t3", 3F, 2, ProductsCategories.Appliances.toString());
+        Product4Id = invMan.addNewProduct("t4", 5F, 6, ProductsCategories.Other.toString());;
+    }
+
 }
