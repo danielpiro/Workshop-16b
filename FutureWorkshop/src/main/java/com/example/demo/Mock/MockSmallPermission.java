dc39a34e5d4608a37a10dbec694cb470e3fe1817
amitpeled92 <62392209+amitpeled92@users.noreply.github.com>
2022-06-07 15:41:23 +0300

    mock tries with daniel

--- FutureWorkshop/src/main/java/com/example/demo/Mock/MockSmallPermission.java
+++ FutureWorkshop/src/main/java/com/example/demo/Mock/MockSmallPermission.java
@@ -3,12 +3,16 @@ package com.example.demo.Mock;
 import com.example.demo.StorePermission.Permission;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
+@Setter
+@NoArgsConstructor
 public class MockSmallPermission {
 
     @NotNull
